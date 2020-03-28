bba7eac4c3211dcf023b7519f169f91765234fd8
Sourabh-prog <60166065+Sourabh-prog@users.noreply.github.com>
2020-03-28 12:28:40 +0530

    My first Github Prg

---
+++ Myfile.py
@@ -0,0 +1,16 @@
+//My first Test Program for arithmetic perations:
+
+num1 = input("insert number 1- ") //Input: Add first Number
+num2 = input("insert number 2- ") //Input: Add 2nd Number
+n1=float(num1) //number or string that needs to be converted to floating point numberIf it's a string, the string should contain decimal points
+n2=float(num2) 
+Add = n1+n2
+Sub = n1-n2
+mul = n1*n2 
+div = n1/n2
+rem = n1%n2
+print(Add)
+print(Sub)
+print(mul)
+print(div)
+print(rem)
